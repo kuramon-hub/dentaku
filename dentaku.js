27277aecf574a1222e7796e93ddf471c33766db6
EC2 Default User <ec2-user@ip-172-31-12-72.us-east-2.compute.internal>
2021-11-10 14:55:02 +0000

    不正表示防止設定済み

--- dentaku.js
+++ dentaku.js
@@ -1,7 +1,41 @@
     var result = document.getElementById("result");
     function edit(elem) {
-      result.value = result.value + elem.value;
-    }
+      
+      //末尾が０の状態で数値が入力できない
+
+        var resultNum = result.value;
+        
+      
+      if(resultNum.slice( -1 )  == "."){
+        result.value = result.value + elem.value;
+      }else if(resultNum.slice( 0, 1 )  == 0 ){
+        if(elem.value == "."){
+        result.value = result.value + elem.value;
+        }else {
+
+          result.value = elem.value;
+        }
+      }else if(resultNum.slice( -2 ) == "+0"){
+          result.value = resultNum.slice(0,-1);
+          result.value = result.value + elem.value;
+      }else if(resultNum.slice( -2 ) == "-0"){
+          result.value = result.value + elem.value;
+      }else if(resultNum.slice( -2 ) == "/0"){
+          result.value = result.value + elem.value;
+      }else if(resultNum.slice( -2 ) == "*0"){
+          result.value = result.value + elem.value;
+      }else{
+        result.value = result.value + elem.value;
+      }
+      }
+
+
+
+      
+      
+    
+
+    
     
     function ac() {
       result.value = 0;
